238afbe90a8b53d1e7e2d624860e0e02b76700ee
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2024-12-25 21:33:08 -0300

    upgrade: simplify nix flake and update elixir

--- flake.nix
+++ flake.nix
@@ -1,43 +1,40 @@
 {
   description = "Supabase SDK for Elixir";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-  outputs = {
-    flake-parts,
-    systems,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = import systems;
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        inherit (pkgs.beam.interpreters) erlang_27;
-        inherit (pkgs.beam) packagesWith;
-        beam = packagesWith erlang_27;
-      in {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs.lib) genAttrs;
+    inherit (nixpkgs.lib.systems) flakeExposed;
+    forAllSystems = f:
+      genAttrs flakeExposed (system: f (import nixpkgs {inherit system;}));
+  in {
+    devShells = forAllSystems (pkgs: let
+      inherit (pkgs) mkShell;
+      inherit (pkgs.beam.interpreters) erlang_27;
+      inherit (pkgs.beam) packagesWith;
+      beam = packagesWith erlang_27;
+      elixir_1_18 = beam.elixir.override {
+        version = "1.18.1";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "elixir-lang";
+          repo = "elixir";
+          rev = "v1.18.1";
+          sha256 = "sha256-zJNAoyqSj/KdJ1Cqau90QCJihjwHA+HO7nnD1Ugd768=";
         };
-        devShells.default = with pkgs;
-          mkShell {
-            name = "postgrest-ex";
-            packages = with pkgs;
-              [beam.elixir_1_17]
-              ++ lib.optional stdenv.isLinux [inotify-tools]
-              ++ lib.optional stdenv.isDarwin [
-                darwin.apple_sdk.frameworks.CoreServices
-                darwin.apple_sdk.frameworks.CoreFoundation
-              ];
-          };
       };
-    };
+    in {
+      default = mkShell {
+        name = "postgrest-ex";
+        packages = with pkgs;
+          [elixir_1_18]
+          ++ lib.optional stdenv.isLinux [inotify-tools]
+          ++ lib.optional stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.CoreServices
+            darwin.apple_sdk.frameworks.CoreFoundation
+          ];
+      };
+    });
+  };
 }
