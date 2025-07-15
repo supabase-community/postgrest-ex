4c5ef3b06363c5eff32f24aabb24b16dfb80821d
zoey <zoey.spessanha@zeetech.io>
2025-07-15 19:59:59 -0300

    config: release-please (#17)
    Release-As: 1.0.0

--- flake.nix
+++ flake.nix
@@ -1,39 +1,32 @@
 {
-  description = "Supabase SDK for Elixir";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05-small";
+    elixir-overlay.url = "github:zoedsoupe/elixir-overlay";
+  };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    nixpkgs,
+    elixir-overlay,
+    ...
+  }: let
     inherit (nixpkgs.lib) genAttrs;
     inherit (nixpkgs.lib.systems) flakeExposed;
     forAllSystems = f:
-      genAttrs flakeExposed (system: f (import nixpkgs {inherit system;}));
+      genAttrs flakeExposed (system:
+        f (import nixpkgs {
+          inherit system;
+          overlays = [elixir-overlay.overlays.default];
+        }));
   in {
     devShells = forAllSystems (pkgs: let
       inherit (pkgs) mkShell;
       inherit (pkgs.beam.interpreters) erlang_27;
-      inherit (pkgs.beam) packagesWith;
-      beam = packagesWith erlang_27;
-      elixir_1_18 = beam.elixir.override {
-        version = "1.18.1";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "elixir-lang";
-          repo = "elixir";
-          rev = "v1.18.1";
-          sha256 = "sha256-zJNAoyqSj/KdJ1Cqau90QCJihjwHA+HO7nnD1Ugd768=";
-        };
-      };
     in {
       default = mkShell {
         name = "postgrest-ex";
         packages = with pkgs;
-          [elixir_1_18 postgresql]
-          ++ lib.optional stdenv.isLinux [inotify-tools]
-          ++ lib.optional stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.CoreServices
-            darwin.apple_sdk.frameworks.CoreFoundation
-          ];
+          [elixir-bin."1.18.4" erlang_27 postgresql]
+          ++ lib.optional stdenv.isLinux [inotify-tools];
       };
     });
   };
