9fef756bdf1a0dbf410c404564fac58ea1b543ba
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-09-08 15:29:37 -0300

    feat: aggregation function helpers

--- lib/supabase/postgrest/query_builder/behaviour.ex
+++ lib/supabase/postgrest/query_builder/behaviour.ex
@@ -3,6 +3,8 @@ defmodule Supabase.PostgREST.QueryBuilder.Behaviour do
 
   alias Supabase.Fetcher.Request
 
+  @type column :: String.t() | atom
+  @type agg_opt :: {:as, column}
   @type options :: [count: :exact, returning: boolean | :representation]
   @type insert_options :: [count: :exact, returning: boolean | :representation, on_conflict: any]
 
@@ -16,4 +18,11 @@ defmodule Supabase.PostgREST.QueryBuilder.Behaviour do
   @callback delete(Request.t(), options) :: Request.t()
   @callback update(Request.t(), map) :: Request.t()
   @callback update(Request.t(), map, options) :: Request.t()
+
+  # aggregate functions helpers
+  @callback sum(column, list(agg_opt)) :: String.t()
+  @callback max(column, list(agg_opt)) :: String.t()
+  @callback min(column, list(agg_opt)) :: String.t()
+  @callback avg(column, list(agg_opt)) :: String.t()
+  @callback count(column, list(agg_opt)) :: String.t()
 end
