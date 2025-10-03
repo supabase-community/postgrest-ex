057f0f8868740cf24dd7d0e16d4f59df54798d5b
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-10-02 23:34:23 -0300

    chore: fix tests

--- lib/supabase/postgrest/schema_decoder.ex
+++ lib/supabase/postgrest/schema_decoder.ex
@@ -10,12 +10,30 @@ defmodule Supabase.PostgREST.SchemaDecoder do
   def decode(%Response{} = resp, opts \\ []) do
     schema = Keyword.fetch!(opts, :schema)
 
-    with {:ok, body} <- JSONDecoder.decode(resp, keys: :atoms) do
+    with {:ok, body} <- JSONDecoder.decode(resp, []) do
+      body = atom_keys(body)
+
       cond do
-        resp.status < 400 and is_list(body) -> {:ok, Enum.map(body, &struct(schema, &1))}
-        resp.status < 400 -> {:ok, struct(schema, body)}
-        true -> {:ok, body}
+        resp.status < 400 and is_list(body) ->
+          {:ok, Enum.map(body, &struct(schema, &1))}
+
+        resp.status < 400 ->
+          {:ok, struct(schema, body)}
+
+        true ->
+          {:ok, body}
       end
     end
+  end
+
+  defp atom_keys(body) when is_list(body) do
+    Enum.map(body, &atom_keys/1)
+  end
+
+  defp atom_keys(body) when is_map(body) do
+    Map.new(body, fn
+      {k, v} when is_map(v) -> {String.to_atom(k), atom_keys(v)}
+      {k, v} -> {String.to_atom(k), v}
+    end)
   end
 end
