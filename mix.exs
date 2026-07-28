fa45d8b24001374feba9303e2e872feaf65ead4c
Zoey Pessanha <zoey.spessanha@zeetech.io>
2026-07-28 00:34:43 -0300

    deps: update supabase potion to 1.0.0

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule PostgREST.MixProject do
     if System.get_env("SUPABASE_LOCAL") == "1" do
       {:supabase_potion, path: "../supabase-ex"}
     else
-      {:supabase_potion, "~> 0.7"}
+      {:supabase_potion, "~> 1.0.0"}
     end
   end
 
